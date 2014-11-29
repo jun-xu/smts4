0b2d2440eeeea48e327c858beb12840bba3caf2f
ss <ss@192.168.1.103>
2014-11-29 19:07:55 +0800

    流媒体测试模块，用于 测试客户端

---
+++ src/test_smts_client.c
@@ -0,0 +1,239 @@
+/*
+ * test_smts_client.c
+ *
+ *  Created on: 上午9:51:02
+ *      Author: ss
+ */
+
+#include "test_smts_client.h"
+#include "smts_proto.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include "css_logger.h"
+#include "tcp_server.h"
+#include "encode_decode_util.h"
+#include "test_mock_dvr.h"
+#include "smts_client_impl.h"
+#include <assert.h>
+
+/**
+ * for mem watch.
+ */
+#include "mem_guard.h"
+static int init_test_smts_client(uv_loop_t *loop, test_smts_client_t *client)
+{
+	init_abstract_tcp_client((abstract_tcp_client_t*) client, loop, PACK0);
+	client->cur_frame_seq = -1;
+	client->status = 0;
+	client->recv_frame_account = 0;
+	return 0;
+}
+
+static void test_smts_client_close_cb(abstract_tcp_client_t* aclient, int status)
+{
+	test_smts_client_t *test_client = (test_smts_client_t*) aclient;
+	FREE(test_client);
+}
+
+static int test_client_on_read_packet(abstract_tcp_client_t *client, uv_buf_t *packet, int status)
+{
+//	assert(0 == status);
+	test_smts_client_t *test_client = (test_smts_client_t*) client;
+	if (status != 0) {
+		return status;
+	}
+
+	int cmd = decode_int32((packet)->base + 4);
+//	printf("client:%d recv cmd:%x status:%d\n", client->socket.io_watcher.fd, cmd, test_client->status);
+	if (test_client->status == 0) {
+		///read preview response.
+		CHECK_RES_CMD(PREVIEW_RES_CMD, packet);
+		preview_cmd_res_t *res = (preview_cmd_res_t*) malloc(sizeof(preview_cmd_res_t));
+		assert(0 == preview_cmd_res_t_init(res));
+		assert(0 == preview_cmd_res_t_decode(packet, res));
+		CL_DEBUG("read preview response,status:%d.\n", res->status);
+		assert(0 == res->status);
+		test_client->status = 1;
+		preview_cmd_res_t_destroy(res);
+	} else {
+		/// read frame
+		CHECK_RES_CMD(SEND_FRAME_CMD, packet);
+		smts_frame_res_t *frame = (smts_frame_res_t*) malloc(sizeof(smts_frame_res_t));
+		assert(0 == smts_frame_res_t_init(frame));
+		assert(0 == smts_frame_res_t_decode(packet, frame));
+//		CL_DEBUG("test client read frame:%d, status:%d\n", frame->seqno, status);
+		test_client->recv_frame_account++;
+		if (test_client->cur_frame_seq == -1) {
+			test_client->cur_frame_seq = frame->seqno;
+		} else {
+			assert(++test_client->cur_frame_seq == frame->seqno);
+		}
+		smts_frame_res_t_destroy(frame);
+		if (test_client->recv_frame_account > TERMINATE_SEQNO) {
+			test_client->socket.data = test_client;
+			CL_DEBUG("test smts client fd:%d closed.\n", test_client->socket.io_watcher.fd);
+			close_abstract_tcp_client(client, test_smts_client_close_cb);
+//			uv_close((uv_handle_t*) &test_client->socket, test_smts_client_close_cb);
+		}
+	}
+	return 0;
+}
+
+static void test_client_on_send_preview_cmd(abstract_tcp_client_t *client, abstract_cmd_t *packet, int status)
+{
+	preview_cmd_t *req = (preview_cmd_t *) packet;
+	preview_cmd_t_destroy(req);
+// 3. recv frame
+	assert(0 == tcp_client_start_read(client, test_client_on_read_packet));
+
+}
+
+static void test_client_on_connected(abstract_tcp_client_t *aclient, int status)
+{
+	CL_DEBUG("test client fd:%d connected:%d\n", aclient->socket.io_watcher.fd, status);
+	test_smts_client_t *client = (test_smts_client_t*) aclient;
+// 2. send preview cmd.
+	preview_cmd_t *req = (preview_cmd_t *) malloc(sizeof(preview_cmd_t));
+	assert(0 == preview_cmd_t_init(req));
+	req->src_addr = 2;
+	req->dest_addr = 3;
+	req->seqno = 4;
+	req->dvr_id = client->dvr_id;
+	req->channel_no = 6;
+	memcpy(req->token, "token", sizeof("token"));
+	req->frame_mode = 7;
+	req->packet_len = preview_cmd_t_len(req);
+// alloc send bufs;
+	assert(0 == preview_cmd_t_bufs_alloc(req));
+// encode packet
+	assert(0 == preview_cmd_t_encode(req));
+	assert(0 == tcp_client_send_msg(aclient, (abstract_cmd_t* ) req, test_client_on_send_preview_cmd));
+
+}
+
+int test_smts_start_preivew(uv_loop_t *loop, test_smts_client_t *client, int64_t dvr_id)
+{
+	int r = 0;
+// 1. connect to server.
+	assert(0 == init_test_smts_client(loop, client));
+	client->dvr_id = dvr_id;
+	r = tcp_client_connect((abstract_tcp_client_t*) client, "127.0.0.1", DEFAUTL_LISTEN_PORT, test_client_on_connected,
+	PACK0);
+	return r;
+}
+
+void test_struct_encode_decode_suite()
+{
+	int len;
+	char *token = "token";
+	uv_buf_t *packet;
+	preview_cmd_t *req, *req0;
+	req = (preview_cmd_t*) malloc(sizeof(preview_cmd_t));
+	req0 = (preview_cmd_t*) malloc(sizeof(preview_cmd_t));
+	assert(0 == preview_cmd_t_init(req));
+	req->src_addr = 2;
+	req->dest_addr = 3;
+	req->seqno = 4;
+	req->dvr_id = 5;
+	req->channel_no = 6;
+	memcpy(req->token, "token", sizeof(token));
+	req->frame_mode = 7;
+
+	len = preview_cmd_t_len(req);
+	req->packet_len = len;
+
+	assert(0 == preview_cmd_t_init(req0));
+	assert(0 == preview_cmd_t_bufs_alloc(req));
+	assert(0 == preview_cmd_t_encode(req));
+	assert(1 == req->original_buf_len);
+	packet = req->original_buf;
+	req->original_buf = NULL;
+	req->original_buf_len = 0;
+	assert(0 == preview_cmd_t_decode(packet, req0));
+	assert(req0->packet_len == req->packet_len);
+	assert(req0->cmd == req->cmd);
+	assert(req0->src_addr == req->src_addr);
+	assert(req0->dest_addr == req->dest_addr);
+	assert(req0->seqno == req->seqno);
+	assert(strcmp(req0->token, req->token) == 0);
+
+	assert(0 == preview_cmd_t_destroy(req));
+	assert(0 == preview_cmd_t_destroy(req0));
+
+}
+/**
+ * test preview.
+ */
+static uv_loop_t loop;
+static uv_loop_t dvr_loop;
+static uv_timer_t timer;
+
+static int repert = 1;
+#define TEST_CLIENT_SIZE 1
+
+#ifdef MEM_GUARD
+static void printf_mem_info(uv_timer_t *t) {
+	printf_all_ptrs();
+}
+#endif
+
+static void smts_client_start_new(uv_timer_t *t)
+{
+	int i;
+	if (TEST_CLIENT_SIZE == 1)
+		return;
+	for (i = 0; i < TEST_CLIENT_SIZE; i++) {
+		test_smts_client_t *test_client = (test_smts_client_t*) malloc(sizeof(test_smts_client_t));
+		assert(0 == test_smts_start_preivew(&loop, test_client, 0));
+	}
+	if (--repert > 0) {
+		uv_timer_start(&timer, smts_client_start_new, 5000, 0);
+	} else {
+#ifdef MEM_GUARD
+		uv_timer_start(&timer,printf_mem_info,2000,0);
+#endif
+	}
+}
+
+static void test_preview_start_tcp_server_cb()
+{
+
+	int i;
+	for (i = 0; i < TEST_CLIENT_SIZE; i++) {
+		test_smts_client_t *test_client = (test_smts_client_t*) malloc(sizeof(test_smts_client_t));
+		assert(0 == test_smts_start_preivew(&loop, test_client, 0));
+	}
+	if (--repert > 0) {
+		uv_timer_start(&timer, smts_client_start_new, 5000, 0);
+	} else {
+#ifdef MEM_GUARD
+		uv_timer_start(&timer,printf_mem_info,2000,0);
+#endif
+	}
+}
+
+void start_mock_dvr_server()
+{
+	uv_loop_init(&dvr_loop);
+	smts_tcp_server_t *tcp_server = (smts_tcp_server_t*) malloc(sizeof(smts_tcp_server_t));
+	assert(0 == start_mock_dvr(tcp_server,&dvr_loop,DEFAULT_MOCK_DVR_PORT));
+	uv_run(&dvr_loop, UV_RUN_DEFAULT);
+	stop_tcp_server(tcp_server, NULL);
+	destroy_tcp_server(tcp_server);
+}
+
+void test_preview_suite()
+{
+	uv_thread_t thread_t;
+	uv_thread_create(&thread_t, start_mock_dvr_server, NULL);
+	Sleep(500);
+	uv_loop_init(&loop);
+	uv_timer_init(&loop, &timer);
+	smts_tcp_server_t *tcp_server = (smts_tcp_server_t*) malloc(sizeof(smts_tcp_server_t));
+	assert(0==init_tcp_server(tcp_server,&loop,DEFAUTL_LISTEN_PORT));
+	assert(0 == start_tcp_server(tcp_server, test_preview_start_tcp_server_cb, client_on_connection));
+	destroy_tcp_server(tcp_server);
+	uv_run(&loop, UV_RUN_DEFAULT);
+	uv_thread_join(&thread_t);
+}
+
---
+++ src/test_smts_client.h
@@ -0,0 +1,30 @@
+/*
+ * test_smts_client.h
+ *
+ *  Created on: 上午9:50:51
+ *      Author: ss
+ */
+
+#ifndef TEST_SMTS_CLIENT_H_
+#define TEST_SMTS_CLIENT_H_
+
+#include "uv/uv.h"
+#include "smts_abstract_tcp_client.h"
+
+
+#define CHECK_RES_CMD(cmd,buf) assert((cmd) == decode_int32((buf)->base+4))
+
+#define TERMINATE_SEQNO 2
+
+typedef struct test_smts_client_s
+{
+	ABSTRACT_TCP_CLIENT_FILEDS
+	int64_t dvr_id;
+	int cur_frame_seq;
+	int recv_frame_account;
+	int status;
+} test_smts_client_t;
+
+int test_smts_start_preivew(uv_loop_t *loop, test_smts_client_t *client, int64_t dvr_id);
+
+#endif /* TEST_SMTS_CLIENT_H_ */
